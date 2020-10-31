22e16ffb742e80d9a5f4f956cb838770875f41ef
cboudereau <clement.boudereau@availpro.com>
2020-10-31 19:13:50 +0100

    generic impl

--- src/graph.rs
+++ src/graph.rs
@@ -1,59 +1,60 @@
 use std::rc::Rc;
 use std::cell::RefCell;
 use std::collections::HashMap;
+use std::hash::Hash;
 
 #[derive(Debug)]
-pub struct Node {
-    phone: u64,
-    following: Vec<Rc<RefCell<Node>>>,
-    followers: Vec<Rc<RefCell<Node>>>,
+pub struct Node<T> {
+    value: T,
+    incoming: Vec<Rc<RefCell<Node<T>>>>,
+    outgoing: Vec<Rc<RefCell<Node<T>>>>,
 }
 
-impl Node {
-    fn new(datum: u64) -> Rc<RefCell<Node>> {
+impl<T> Node<T> {
+    fn new(value: T) -> Rc<RefCell<Node<T>>> {
         Rc::new(RefCell::new(Node {
-            phone: datum,
-            following: Vec::new(),
-            followers: Vec::new()
+            value: value,
+            incoming: Vec::new(),
+            outgoing: Vec::new()
         }))
     }
 }
 
-pub type Roots = HashMap<u64, Rc<RefCell<Node>>>;
+pub type Roots<T> = HashMap<T, Rc<RefCell<Node<T>>>>;
 
-pub trait RootsFeatures {
+pub trait RootsFeatures<T> {
     fn with_capacity(capacity:usize) -> Self;
-    fn add_edge(&mut self, x:u64, y:u64) -> &Self;
-    fn lookup(&mut self, x:u64) -> Option<Vec<u64>>;
-    fn rlookup(&mut self, x:u64) -> Option<Vec<u64>>;
+    fn add_edge(&mut self, x:T, y:T) -> &Self;
+    fn lookup(&mut self, x:T) -> Option<Vec<T>>;
+    fn rlookup(&mut self, x:T) -> Option<Vec<T>>;
 }
 
-impl RootsFeatures for Roots {
+impl<T:Eq + Hash + Copy> RootsFeatures<T> for Roots<T> {
     fn with_capacity(capacity:usize) -> Self {
         HashMap::with_capacity(capacity)
     }
 
-    fn add_edge(&mut self, x:u64, y:u64) -> &Self {
-        let nx : &Rc<RefCell<Node>> = &self.entry(x).or_insert_with(|| Node::new(x)).clone();
-        let ny : &Rc<RefCell<Node>> = &self.entry(y).or_insert_with(|| Node::new(y)).clone();
+    fn add_edge(&mut self, x:T, y:T) -> &Self {
+        let nx : &Rc<RefCell<Node<T>>> = &self.entry(x).or_insert_with(|| Node::new(x)).clone();
+        let ny : &Rc<RefCell<Node<T>>> = &self.entry(y).or_insert_with(|| Node::new(y)).clone();
         
         {
             let mut mut_x = nx.borrow_mut();
             let mut mut_y = ny.borrow_mut();
-            &mut_x.following.push(ny.clone());
-            &mut_y.followers.push(nx.clone());
+            &mut_x.incoming.push(ny.clone());
+            &mut_y.outgoing.push(nx.clone());
         }
         
         self
     }
 
-    fn lookup(&mut self, x:u64) -> Option<Vec<u64>> {
+    fn lookup(&mut self, x:T) -> Option<Vec<T>> {
         let x = &self.get(&x)?;
-        Some (x.borrow().following.iter().map(|x| x.borrow().phone).collect::<Vec<_>>())
+        Some (x.borrow().incoming.iter().map(|x| x.borrow().value).collect::<Vec<_>>())
     }
 
-    fn rlookup(&mut self, x:u64) -> Option<Vec<u64>> {
+    fn rlookup(&mut self, x:T) -> Option<Vec<T>> {
         let x = &self.get(&x)?;
-        Some (x.borrow().followers.iter().map(|x| x.borrow().phone).collect::<Vec<_>>()) 
+        Some (x.borrow().outgoing.iter().map(|x| x.borrow().value).collect::<Vec<_>>()) 
     }
 }
