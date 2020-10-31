78a7ca6962bfd0a328d1f176cf1fe745735ff6b0
cboudereau <clement.boudereau@availpro.com>
2020-10-31 21:38:58 +0100

    typo

--- tests/lib.rs
+++ tests/lib.rs
@@ -8,43 +8,43 @@ mod tests {
     fn from_sample_lookup_and_rlookup() {
         let mut graph = GraphMap::with_capacity(100000);
         
-        let phone_x = 1;
-        let phone_y = 2;
-        let phone_z = 3;
-        let phone_p = 4;
+        let x = 1;
+        let y = 2;
+        let z = 3;
+        let p = 4;
         
         // x -> y
-        &graph.add_edge(phone_x, phone_y);
+        &graph.add_edge(x, y);
         
         // x -> z
-        &graph.add_edge(phone_x, phone_z);
+        &graph.add_edge(x, z);
         
         // y -> x
-        &graph.add_edge(phone_y, phone_x);
+        &graph.add_edge(y, x);
 
         // y -> z
-        &graph.add_edge(phone_y, phone_z);
+        &graph.add_edge(y, z);
 
         // p -> x
-        &graph.add_edge(phone_p, phone_x);
+        &graph.add_edge(p, x);
 
         // assert x
-        assert_eq!(&Some (vec! [2, 3]), &graph.lookup(phone_x));
-        assert_eq!(&Some (vec! [2, 4]), &graph.rlookup(phone_x));
+        assert_eq!(&Some (vec! [y, z]), &graph.lookup(x));
+        assert_eq!(&Some (vec! [y, p]), &graph.rlookup(x));
 
         // assert y
-        assert_eq!(&Some (vec! [1, 3]), &graph.lookup(phone_y));       
-        assert_eq!(&Some (vec! [1]), &graph.rlookup(phone_y));       
+        assert_eq!(&Some (vec! [x, z]), &graph.lookup(y));       
+        assert_eq!(&Some (vec! [x]), &graph.rlookup(y));       
 
         // assert z
-        assert_eq!(&Some (vec! []), &graph.lookup(phone_z));
-        assert_eq!(&Some (vec! [1, 2]), &graph.rlookup(phone_z));
+        assert_eq!(&Some (vec! []), &graph.lookup(z));
+        assert_eq!(&Some (vec! [x, y]), &graph.rlookup(z));
 
         // assert p
-        assert_eq!(&Some (vec! [1]), &graph.lookup(phone_p));
-        assert_eq!(&Some (vec! []), &graph.rlookup(phone_p));
+        assert_eq!(&Some (vec! [x]), &graph.lookup(p));
+        assert_eq!(&Some (vec! []), &graph.rlookup(p));
 
         // p can follow x and y since p -> x -> y (2) -> z (3)
-        assert_eq!(&Some(vec!(2, 3).into_iter().collect()), &graph.suggest(phone_p).map(|x| x.into_iter().collect::<HashSet<_>>()));
+        assert_eq!(&Some(vec!(y, z).into_iter().collect()), &graph.suggest(p).map(|x| x.into_iter().collect::<HashSet<_>>()));
     }
 }
